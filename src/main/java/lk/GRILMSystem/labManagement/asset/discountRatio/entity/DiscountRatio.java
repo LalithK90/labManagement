e7a9e6a1d3b93b6c2fd46e133d01eaf4376a0e0b
LalithK90 <asakahatapitiya@gmail.com>
2020-08-02 10:13:25 +0530

    DiscountRatio radio was added

---
+++ src/main/java/lk/GRILMSystem/labManagement/asset/discountRatio/entity/DiscountRatio.java
@@ -0,0 +1,36 @@
+package lk.GRILMSystem.labManagement.asset.discountRatio.entity;
+
+import lk.GRILMSystem.labManagement.asset.discountRatio.entity.Enum.DiscountRatioStatus;
+import lombok.*;
+
+import javax.persistence.*;
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class DiscountRatio {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Integer id;
+
+
+    private String name;
+
+
+    @Column( nullable = false, precision = 10, scale = 2 )
+    private BigDecimal amount;
+
+
+    private DiscountRatioStatus discountRatioStatus;
+
+/*    @OneToMany
+    @JoinColumn(name = "discount_ratio_id")
+    private List<Invoice> invoices = new ArrayList<>();*/
+
+
+}
