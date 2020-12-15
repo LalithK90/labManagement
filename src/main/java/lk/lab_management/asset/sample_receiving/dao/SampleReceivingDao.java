f2e1e5e1148fa27472b9c4e8ea0595908c3eef43
LalithK90 <asakahatapitiya@gmail.com>
2020-12-15 09:35:15 +0530

    SampleReceiving to add new variable as number

--- src/main/java/lk/lab_management/asset/sample_receiving/dao/SampleReceivingDao.java
+++ src/main/java/lk/lab_management/asset/sample_receiving/dao/SampleReceivingDao.java
@@ -9,5 +9,6 @@ import java.util.List;
 
 @Repository
 public interface SampleReceivingDao extends JpaRepository< SampleReceiving, Integer > {
+SampleReceiving findFirstByOrderByIdDesc();
   List<SampleReceiving> findBySampleReceivingStatus(SampleReceivingStatus sampleReceivingStatus);
 }
